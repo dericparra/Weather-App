4bce5b039d4263673a7c3770922519c24f59ad0f
Deric Parra <parradeko@gmail.com>
2020-02-19 11:12:38 -0300

    Updates

---
+++ precache-manifest.8497caf969407b5d27d8259a1947b253.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09b3f439e8de049764ad86eac552ea10",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "9e53c56c0ce6c3e180c5",
+    "url": "/Weather-App/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b62c7c295421a21c7cad",
+    "url": "/Weather-App/static/js/2.6e7b1102.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Weather-App/static/js/2.6e7b1102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e53c56c0ce6c3e180c5",
+    "url": "/Weather-App/static/js/main.659a00cb.chunk.js"
+  },
+  {
+    "revision": "5da67bea8e2fd6a8881a",
+    "url": "/Weather-App/static/js/runtime-main.ec6f3823.js"
+  }
+]);
